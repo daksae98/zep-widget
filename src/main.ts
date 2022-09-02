5685fc1c6302b073dbb1e877c665f044ee17befc
qudwnbj <walpnv@gmail.com>
2022-09-02 14:57:17 +0900

    Add: 질문추가

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,172 @@
 import "styles/global.css";
 import "styles/main.css";
 
+let step = 0;
+let correctAnswerCount = 0;
+
 const doc =
-  typeof window !== undefined && typeof document !== "undefined"
+  typeof window !== "undefined" && typeof document !== "undefined"
     ? document
     : null;
 
 const trueButton = doc?.querySelector("#true-button");
 const falseButton = doc?.querySelector("#false-button");
+const mainQuestion = doc?.querySelector("#main-question");
+const mainBoolQuestion = doc?.querySelector("#main-bool-question");
+const trueButtonText = doc?.querySelector("#true-button-text");
+const falseButtonText = doc?.querySelector("#false-button-text");
+const correctAnswer = doc?.querySelector("#correct-answer");
+const incorrectAnswer = doc?.querySelector("#incorrect-answer");
+
+const setInnerHTML = (element: Element | null | undefined, text: string) => {
+  if (element) {
+    element.innerHTML = text;
+  }
+};
 
 const removeText = () => {
-  const mainQuestion = doc?.querySelector("#main-question");
-  const trueButtonText = doc?.querySelector("#true-button-text");
-  const falseButtonText = doc?.querySelector("#false-button-text");
+  setInnerHTML(mainQuestion, "");
+  setInnerHTML(mainBoolQuestion, "");
+  setInnerHTML(trueButtonText, "");
+  setInnerHTML(falseButtonText, "");
+};
 
-  if (mainQuestion) {
-    mainQuestion.innerHTML = "";
-  }
-  if (trueButtonText) {
-    trueButtonText.innerHTML = "";
-  }
-  if (falseButtonText) {
-    falseButtonText.innerHTML = "";
-  }
+const setQuestionMode = () => {
+  step += 1;
+  setInnerHTML(trueButtonText, "True");
+  setInnerHTML(falseButtonText, "False");
+  setInnerHTML(mainBoolQuestion, "True? Or False?");
+  setInnerHTML(correctAnswer, "");
+  setInnerHTML(incorrectAnswer, "");
+};
+
+const correct = () => {
+  setInnerHTML(
+    correctAnswer,
+    '<div class="result-backdrop"><div class="correct-circle" /></div>'
+  );
+  correctAnswerCount += 1;
+};
+
+const incorrect = (text: string) => {
+  setInnerHTML(
+    incorrectAnswer,
+    `<div class="result-backdrop"><p class="incorrect">Incorrect!</p><p class="text">${text}</p></div>`
+  );
 };
 
 trueButton?.addEventListener("click", () => {
   removeText();
-  const correctAnswer = doc?.querySelector("#correct-answer");
-  if (correctAnswer) {
-    correctAnswer.innerHTML = `
-    <div class="result-backdrop">
-      <div class="correct-circle" />
-    </div>`;
+  switch (step) {
+    case 0: {
+      correct();
+      setTimeout(() => {
+        setQuestionMode();
+        setInnerHTML(mainQuestion, "Amelia likes summer because it is warm");
+      }, 2000);
+      break;
+    }
+    case 1: {
+      incorrect("David likes summer because he can go swimming");
+      setTimeout(() => {
+        setQuestionMode();
+        setInnerHTML(
+          mainQuestion,
+          "David don’t like winter because it is cold"
+        );
+      }, 2000);
+      break;
+    }
+    case 2: {
+      correct();
+      setTimeout(() => {
+        setQuestionMode();
+        setInnerHTML(
+          mainQuestion,
+          "Amelia likes winter because she can go skiing"
+        );
+      }, 2000);
+      break;
+    }
+    case 3: {
+      correct();
+      setTimeout(() => {
+        setQuestionMode();
+        setInnerHTML(
+          mainQuestion,
+          "Amelia suggests to book ski passes in the winter"
+        );
+      }, 2000);
+      break;
+    }
+    case 4: {
+      incorrect(
+        "Amelia suggests to book ski passes in the fall not in the winter"
+      );
+      setTimeout(() => {
+        setInnerHTML(
+          incorrectAnswer,
+          `<div class="result-backdrop"><p class="incorrect">총 ${correctAnswerCount}문제 맞췄습니다!</p></div>`
+        );
+      }, 2000);
+      break;
+    }
   }
 });
 
 falseButton?.addEventListener("click", () => {
   removeText();
-  const incorrectAnswer = doc?.querySelector("#incorrect-answer");
-  if (incorrectAnswer) {
-    incorrectAnswer.innerHTML = `
-    <div class="result-backdrop">
-      <p class="incorrect">Incorrect!</p>
-      <p class="text">David likes summer because he can go swimming</p>
-    </div>`;
+  switch (step) {
+    case 0: {
+      incorrect("David likes summer because he can go swimming");
+      setTimeout(() => {
+        setQuestionMode();
+        setInnerHTML(mainQuestion, "Amelia likes summer because it is warm");
+      }, 2000);
+      break;
+    }
+    case 1: {
+      correct();
+      setTimeout(() => {
+        setQuestionMode();
+        setInnerHTML(
+          mainQuestion,
+          "David don’t like winter because it is cold"
+        );
+      }, 2000);
+      break;
+    }
+    case 2: {
+      incorrect("David don’t like winter because it is cold");
+      setTimeout(() => {
+        setQuestionMode();
+        setInnerHTML(
+          mainQuestion,
+          "Amelia likes winter because she can go skiing"
+        );
+      }, 2000);
+      break;
+    }
+    case 3: {
+      incorrect("Amelia likes winter because she can go skiing");
+      setTimeout(() => {
+        setQuestionMode();
+        setInnerHTML(
+          mainQuestion,
+          "Amelia suggests to book ski passes in the winter"
+        );
+      }, 2000);
+      break;
+    }
+    case 4: {
+      correct();
+      setTimeout(() => {
+        setInnerHTML(
+          correctAnswer,
+          `<div class="result-backdrop"><p class="incorrect">총 ${correctAnswerCount}문제 맞췄습니다!</p></div>`
+        );
+      }, 2000);
+      break;
+    }
   }
 });
